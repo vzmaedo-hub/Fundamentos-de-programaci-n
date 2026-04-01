d8cc2e6f325dd8b4c9bf7bb25343e03d7238683d
vzmaedo-hub <vzmaedo@gmail.com>
2026-04-01 14:14:27 -0300

    mi commit

---
+++ Mis codigos/For.py
@@ -0,0 +1,23 @@
+#por cada_elemento_dentro_de dentro de rango(5) 5 siendo el lìmite, en este caso contaria desde el 0 al 4, no hasta el 5
+for cada_elemento_dentro_de in range(10):
+    print(f"hola, vamos en el numero {cada_elemento_dentro_de}")
+print("")
+print()
+
+for cada_elemento_dentro_de in range(10, 20):
+    print(f"hola, vamos en el numero {cada_elemento_dentro_de}")
+print("")
+print()
+
+for cada_elemento_dentro_de in range(10, 20, 2):
+    print(f"hola, vamos en el numero {cada_elemento_dentro_de}")
+print("")
+print()
+
+for i in range(6):
+    if i % 2 == 0:
+        continue
+    print(i)
+print("")
+print()
+
