d0cb63541c5f36a7a3c09e4da925095c58e022d4
vzmaedo-hub <vzmaedo@gmail.com>
2026-04-06 12:41:32 -0400

    aprendì algo nuevo

---
+++ Mis codigos/Tareas/Gastos_simples.py
@@ -0,0 +1,16 @@
+almuerzo = int(input("¿Cuanto dinero gastas diariamente en el almuerzo?"))
+impresiones = int(input("¿cuántas impresiones realiza al día?")) 
+gasto_semanal = (almuerzo * 5) + (impresiones * 5 * 50)
+print(f"Su gasto semanal es de {gasto_semanal} pesos")
+
+if gasto_semanal > 20000:
+    print("Alerta: Presupuesto alto")
+else:
+    print("Pesupuesto moderado")
+print("Fin")
+
+
+
+
+
+
