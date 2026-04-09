447a1c4ec9f04f57f60005b36b227d5ba82831cd
vzmaedo-hub <vzmaedo@gmail.com>
2026-04-09 11:03:29 -0400

    aprendì algo nuevo

---
+++ Mis codigos/Tareas/Las_reinas_del_k_pop.py
@@ -0,0 +1,21 @@
+
+
+while True:
+    grupo = input("Elije entre bts, stray kids y blackpink").lower()
+    if grupo == "bts":
+        print(f"Bienvenido al grupo de {grupo}")
+        break
+    elif grupo == "blackpink":
+        print(f"Bienvenido al grupo de {grupo}")
+        break
+    elif grupo == "stray kids":
+        print(f"Bienvenido al grupo de {grupo}")
+        break
+    while grupo != "bts" and grupo != "blackpink" and grupo != "stray kids":
+        print("opcion no valida")
+        break
+
+    
+
+
+
---
+++ Mis codigos/Tareas/manu_crafteo_minecraft.py
@@ -0,0 +1,19 @@
+
+print("Hola, bienvenido a la mesa de crafteo")
+
+while True:
+    opcion_crafteo = input("A continuaciòn te dirè las opciones 1).Espada de diamante 2).Pico de diamante 3).Salir de la mesa")
+    if opcion_crafteo == "1":
+        print("Espada crafteada")
+        continue
+    elif opcion_crafteo == "2":
+        print("Pico crafteado")
+        continue
+    elif opcion_crafteo == "3":
+        print("Ha elegido salir de la mesa de crafteo")
+        break
+    else:
+        print("Opcion invàlida")
+
+    
+    
